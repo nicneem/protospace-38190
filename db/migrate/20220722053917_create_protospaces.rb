4084e0b4b23b59a0b3dcabee44a4573e841104c0
nicneem <hiroyuki.t30@gmail.com>
2022-07-22 15:31:08 +0900

    ヘッダーの作成

---
+++ db/migrate/20220722053917_create_protospaces.rb
@@ -0,0 +1,10 @@
+class CreateProtospaces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :protospaces do |t|
+      t.string :name
+      t.string :text
+      t.text :image
+      t.timestamps
+    end
+  end
+end
