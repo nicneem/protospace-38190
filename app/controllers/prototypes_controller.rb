30480d62a7f3ad58f07c4fb8f6fc3e6aa108c353
nicneem <hiroyuki.t30@gmail.com>
2022-07-26 09:57:47 +0900

    投稿機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,24 @@
-class PrototypesController < ApplicationController
-  def 
+class PrototypesController < ApplicationController 
+    def new
+      @prototype= Prototype.new
+    end
+  
+    def create
+      @prototype= Prototype.new(prototypes_params)
+  if @prototype.save
+    redirect_to root_path
+  else
+    render :new
   end
-end
+    
+   
+  end
+
+    private
+
+    def prototypes_params
+      params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+    end
+
+  end
+
